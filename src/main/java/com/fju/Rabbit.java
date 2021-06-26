6abdf8979f625b0648671cad934e63a6f6a46676
409570157 <lovepatty57@gmail.com>
2021-06-26 17:58:20 +0800

    Second

--- src/main/java/com/fju/Rabbit.java
+++ src/main/java/com/fju/Rabbit.java
@@ -3,10 +3,11 @@ package com.fju;
 public class Rabbit extends Thread{
     @Override
     public void run() {
+        setName("Rabbit");
         for (int i = 0; i < 1000; i++) {
-            System.out.println(getName() + " " + i);
+            System.out.println(  "Rabbit  ran " + i + "m");
             if (i ==999){
-                System.out.println(getName()+" is Winner!");
+                System.out.println(getName()+" completed the task !");
             }
         }
     }
--- src/main/java/com/fju/Turtle.java
+++ src/main/java/com/fju/Turtle.java
@@ -4,9 +4,9 @@ public class Turtle implements Runnable{
     @Override
     public void run() {
         for (int i = 0; i < 1000; i++) {
-            System.out.println("R" + " " + i);
+            System.out.println( "Turtle ran " + i +"m");
             if (i ==999){
-                System.out.println("Turtle"+" is Winner!");
+                System.out.println("Turtle completed the task!");
             }
         }
     }
