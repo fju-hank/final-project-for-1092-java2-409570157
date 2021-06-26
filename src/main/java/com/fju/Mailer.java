d6501c6b59956df346c345d59d5c1846a236bd75
409570157 <lovepatty57@gmail.com>
2021-06-26 13:29:10 +0800

    Restart

--- src/main/java/com/fju/Mailer.java
+++
@@ -1,5 +0,0 @@
-package com.fju;
-
-public interface Mailer {
-    public void mail();
-}
--- src/main/java/com/fju/Tester.java
+++ src/main/java/com/fju/Tester.java
@@ -2,6 +2,14 @@ package com.fju;
 
 public class Tester {
     public static void main(String[] args) {
-        System.out.println("Hello");
-    }
-}
+        //Import the horse racing code, which is what I coded when the teacher was teaching.
+        // Rewrite.
+        Horse h1 = new Horse();
+            Horse h2 = new Horse();
+            Horse2 h3 = new Horse2();
+            Thread th = new Thread(h3);
+            th.start();
+            h2.start();
+            h1.start();
+        }}
+
